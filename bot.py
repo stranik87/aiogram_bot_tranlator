7b8911dafdda22e2d596b9d7790a02d183c0a9ff
stranik87 <tulkin.boynazarov@gmail.com>
2023-07-11 11:47:45 +0300

    update

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
-TOKEN = ''
 
+import os
 from translator import tarjimon
 
 from aiogram import Bot,Dispatcher,types
 
 from aiogram.utils import executor
+
+TOKEN = os.environ.get('TOKEN')
+
+
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
@@ -12,7 +16,7 @@ dp = Dispatcher(bot=bot)
 @dp.message_handler(commands='start')
 
 async def start_com(message:types.Message):
-    await message.answer('Botga xush kelibsiz!')
+    await message.answer('Welcome to the translator bot!')
 
 @dp.message_handler(content_types='text')
 async def send_message(message:types.Message):
